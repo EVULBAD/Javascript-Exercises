21df0a50df5ce557d30af5b4bb4f2377545b1b6f
EVULBAD <whywhymeow@gmail.com>
2023-02-04 18:19:33 -0800

    Function removes arguments present in array
    Solution of fourth exercise involved making a function that can remove an unlimited amount of arguments from an array. It also ignores arguments that aren't present in the array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let index;
+    let args = Array.from(arguments);
+    args.shift();
+    for (i = 0; i < args.length; i++) {
+        if (array.indexOf(args[i]) > -1) {
+            index = array.indexOf(args[i]);
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
