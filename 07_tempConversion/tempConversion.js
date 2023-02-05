02f87e8d34427b0b20b5d3b3b91b703adbc3f616
EVULBAD <whywhymeow@gmail.com>
2023-02-04 21:03:36 -0800

    Functions that convert between Fahrenheit and Celsius
    Solution for seventh exercise involved creating two functions, of which can convert Celsius to Fahrenheit and vice versa, rounding each result to one decimal point.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  //(x − 32) × 5/9
+  let celsiusTemp = (temp - 32) * (5/9);
+  celsiusTemp = Number(celsiusTemp.toFixed(1));
+  return celsiusTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  //(x × 9/5 + 32)
+  let fahrenTemp = temp * (9/5) + 32;
+  fahrenTemp = Number(fahrenTemp.toFixed(1));
+  return fahrenTemp;
 };
 
 // Do not edit below this line
