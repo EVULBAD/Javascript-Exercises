7f1a022e8e1dd2e8d5d77dc1d131c249736e0ad7
EVULBAD <whywhymeow@gmail.com>
2023-02-04 19:24:18 -0800

    Function returns sum of every number between range
    Solution to fifth exercise involved making a function that takes two number arguments, then sums up everything between and including them, returning that sum. It also returns "ERROR" if arguments are either negative or not numbers.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
 const sumAll = function() {
-
+    let sum = 0;
+    let maximum;
+    for (i = 0; i < arguments.length; i++) {
+        if (arguments[i] < 0 || (typeof arguments[i] !== 'number')) {
+            return 'ERROR';
+        }
+    }
+    if (arguments[0] > arguments[1]) {
+        maximum = arguments[0];
+    } else {
+        maximum = arguments[1];
+    }
+    for (i = 0; i <= maximum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
