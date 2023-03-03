9a260412d7ba14e70ad3f6b8145c8d507906e558
Dmitry <dmitrydohy@gmail.com>
2023-03-03 15:49:54 -0800

    Functions use two parameters to calculate power and product of array
    Fourth solution for eighth exercise involved writing a function that takes an array parameter and multiplies all of the numbers together.
    Fifth solution for eighth exercise involved writing a function that takes two parameters and, using the first as a base and the second as an exponential, returns the power.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -16,21 +16,31 @@ const sum = function() {
     args = arguments[0];
   for (i = 0; i < args.length; i++) {
     sum += parseInt(args[i]);
-    console.log(parseInt(args[i]));
   }
   return sum;
 };
 
 const multiply = function() {
-
+  let product = 1,
+    args = arguments[0];
+  for (i = 0; i < args.length; i++) {
+    product *= parseInt(args[i]);
+  }
+  return product;
 };
 
 const power = function() {
-	
+  let power = arguments[0] ** arguments[1];
+  return power;
 };
 
 const factorial = function() {
-	
+	let base = arguments[0],
+    factor = 1;
+  for (i = 1; i <= base; i++) {
+    factor *= i;
+  }
+  return factor;
 };
 
 // Do not edit below this line
