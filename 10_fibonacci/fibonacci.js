7b3f50522d764e8401f8fecaa95dcfbe1c23b2f7
Dmitry <dmitrydohy@gmail.com>
2023-03-05 13:43:35 -0800

    Function that returns member of Fibonacci sequence
    Tenth exercise involved writing a function that returns the member of the Fibonacci sequence according to the parameter entered.
    Code could use cleaning up, but it works.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let term1 = 1,
+        term2 = 1,
+        result = 0;
+    if (num < 0) {
+        return "OOPS";
+    } else if (num <= 2) {
+        return 1;
+    } else {
+        for (i = 0; i < num - 2; i++) {
+            result = term1 + term2;
+            term2 = term1;
+            term1 = result;
+        }
+        return result;
+    }
 };
 
 // Do not edit below this line
