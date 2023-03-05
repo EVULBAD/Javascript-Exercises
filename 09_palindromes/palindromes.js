f3e40139ae3864882cbfe0b308771042e87a9ecf
Dmitry <dmitrydohy@gmail.com>
2023-03-05 12:05:50 -0800

    Function checks if string is palindrome
    Ninth exercise involved writing a function that would return true or false depending on whether or not string parameter was a palindrome.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let punctuation = "!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+        word = string.toLowerCase().replace(/[,\s]/g,""),
+        noPunc = word;
+    for (i = 0; i <= word.length; i++) {
+        let chr = word[i];
+        if (punctuation.indexOf(chr) > -1) {
+            noPunc = word.replace(chr,"");
+        };
+    }
+    let result = noPunc.split("").reverse().toString().replaceAll(",","");
+    if (result == noPunc) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
