4160ca78a4c935e1f5ca85d23c75c17c18783a69
Dmitry <dmitrydohy@gmail.com>
2023-03-10 12:44:49 -0800

    Function returns oldest person
    Solution of twelfth exercise involved writing a function that could find the oldest person in an object array by subtracting their birth year from their death year (or current year if death year does not apply).

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = "",
+        birth = 0,
+        death = 0,
+        oldestAge = 0;
+        age = 0,
+        personName = "",
+        currentYear = new Date().getFullYear();
+    for (var x of people) {
+        personName = x.name;
+        birth = x.yearOfBirth;
+        if (x.yearOfDeath == undefined) {
+            age = currentYear - birth;
+        } else { 
+            death = x.yearOfDeath;
+            age = death - birth;
+        }
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldest = x;
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
