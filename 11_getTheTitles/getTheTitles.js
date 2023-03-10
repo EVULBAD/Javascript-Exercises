ce3a73e019a48876074fbde60a39d3c438e264f8
Dmitry <dmitrydohy@gmail.com>
2023-03-10 10:46:42 -0800

    Function returns "title" from array of objects
    Eleventh exercise involved writing a function that returns the "title" property of objects in an array.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = [];
+    for (var x of books) {
+        titles.push(x.title);
+    }
+    return titles;
 };
 
 // Do not edit below this line
